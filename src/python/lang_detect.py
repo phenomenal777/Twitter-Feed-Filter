8cf70157ed95176b75c9376f6dee99d8d7e53600
Ankit Dash <ankitdash2019@gmail.com>
2023-04-07 13:54:26 +0530

    fixed path

--- src/python/lang_detect.py
+++ src/python/lang_detect.py
@@ -1,12 +1,9 @@
 from langdetect import detect
 
-with open("../../go/trends.txt", "r", encoding='UTF-8') as f:
+with open("../../go/files/trends.txt", "r", encoding='UTF-8') as f:
     lines = f.readlines()
 
 english_lines = [line.strip() for line in lines if detect(line) == 'en']
 
-with open("../../go/required_trends.txt", "w", encoding='UTF-8') as f:
+with open("../../go/files/required_trends.txt", "w", encoding='UTF-8') as f:
     f.writelines('\n'.join(english_lines))
-
-
-
