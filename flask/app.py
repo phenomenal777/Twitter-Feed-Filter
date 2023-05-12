2334dfe474abdc9254b0d5f03ca1f4a6af6f242d
Ankit Dash <ankitdash2019@gmail.com>
2023-05-12 18:05:58 +0530

    first commit of frontend

---
+++ flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data')
+def get_data():
+    with open('../go/files/required_trends.txt', 'r') as f:
+        data = f.read().splitlines()
+    return '<br>'.join(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- go/go.mod
+++ go/go.mod
@@ -3,6 +3,6 @@ module github.com/helios2003/twitter-scraper.git
 go 1.20
 
 require (
-	github.com/n0madic/twitter-scraper v0.0.0-20230323164130-5ff4cd0d1ba9 // indirect
-	golang.org/x/net v0.9.0 // indirect
+	github.com/n0madic/twitter-scraper v0.0.0-20230508101308-2b607217fd11 // indirect
+	golang.org/x/net v0.10.0 // indirect
 )
