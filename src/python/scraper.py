ab467d4c3ef9177c23ce9d53bfa4ab22412213d8
Ankit Dash <ankitdash2019@gmail.com>
2023-05-18 02:36:01 +0530

    started with the unsupervised learning model

--- src/python/scraper.py
+++ src/python/scraper.py
@@ -6,6 +6,7 @@ from collections import Counter
 import nltk
 from nltk.corpus import stopwords
 import unicodedata
+import time
 
 # Define the folder path where the text files are located
 folder_path = 'source files/'
@@ -31,31 +32,38 @@ for file_name in os.listdir(folder_path):
 
     # Loop through the URLs and scrape the data
     word_counter = Counter()
-    with open('data/' + file_name, 'a', encoding='UTF-8') as output_file:
+    with open('data/' + "data" + file_name, 'a', encoding='UTF-8') as output_file:
         for url in urls:
             try:
-                # For limiting redirects and setting a timeout of 7 seconds
+        # For limiting redirects and setting a timeout of 7 seconds
                 page = requests.get(url.strip(), allow_redirects=False, timeout=7)
                 soup = BeautifulSoup(page.content, 'html.parser')
                 text = soup.get_text().strip()
 
-                # Remove non-ASCII characters from the text
+        # Remove non-ASCII characters from the text
                 text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
 
                 text = re.sub('\s+', ' ', text)
                 output_file.write(text)
 
-                # Remove stopwords and non-ASCII words from the text
+            # Remove stopwords and non-ASCII words from the text
                 words = ascii_pattern.findall(text)
                 words = [w for w in words if not w.lower() in stop_words]
 
-                # Count the frequency of each word in the text
+            # Count the frequency of each word in the text
                 word_counter.update(words)
 
             except requests.exceptions.Timeout:
                 continue
 
-            # Decrement num_links variable and break out of the loop if it reaches the limit
+            except requests.exceptions.ConnectionError as e:
+            #print(f"Connection error occurred: {e}")
+        # Wait for 5 seconds before retrying the connection
+                time.sleep(5)
+                continue
+
+    # Decrement num_links variable and break out of the loop if it reaches the limit
             num_links -= 1
             if num_links == 0:
                 break
+
