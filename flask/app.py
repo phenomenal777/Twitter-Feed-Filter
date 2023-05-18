985a19863dd0b15c20eda219d40dc1a1943fd9ab
Ankit Dash <ankitdash2019@gmail.com>
2023-05-18 19:50:11 +0530

    added button format to the trends page

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os 
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -14,7 +16,11 @@ def favicon():
 def get_data():
     with open('../go/files/required_trends.txt', 'r') as f:
         data = f.read().splitlines()
-    return '<br>'.join(data)
+    return render_template('data.html', data=data)
+
+@app.route('/redirect_page/<item>')
+def redirect_page(item):
+    return f"data needs to be added"
 
 if __name__ == '__main__':
     app.run(debug=True)
