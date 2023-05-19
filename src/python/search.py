83d97fd7c6d1d178f0c6ba92fbe6598b01723696
Ankit Dash <ankitdash2019@gmail.com>
2023-05-19 21:49:59 +0530

    Revert "added methos for search.py file"
    This reverts commit f194ac7584d87d6d6bb39d61206bfbdf6c3f8baf.

--- src/python/search.py
+++ src/python/search.py
@@ -1,23 +1,18 @@
-import os
 from googlesearch import search
 
-def perform_search():
-    file_path = os.path.join(os.path.dirname(__file__), '..', '..', 'go', 'files', 'required_trends.txt')
+with open('../../go/files/required_trends.txt', 'r') as f:
+    queries = f.read().splitlines()
 
-    with open(file_path, 'r') as f:
-        queries = f.read().splitlines()
+for query in queries:
+    
+    urls = []
+    counter = 0
+   
+    for result in search(query, num_results=10, lang='en'):
+        urls.append(result)
 
-    for query in queries:
-        urls = []
+    with open('source files/{}_urls.txt'.format(query.replace(" ", "_")), 'w', encoding = "UTF-8") as file:
+        for url in urls:
+            file.write(url + '\n')
+        file.close()
 
-        for result in search(query, num_results=10, lang='en'):
-            urls.append(result)
-
-        output_file_path = os.path.join('source files', f'{query.replace(" ", "_")}_urls.txt')
-
-        with open(output_file_path, 'w', encoding="UTF-8") as file:
-            for url in urls:
-                file.write(url + '\n')
-
-# Call the function to perform the search
-#perform_search()
