f147146016ed78a59b80c04dd19f77422d29fc95
Ankit Dash <ankitdash2019@gmail.com>
2023-05-19 18:41:39 +0530

    put the css files into separate files

--- flask/static/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('#trending-btn');
-const dataEl = document.querySelector('#trending-data');
-
-btn.addEventListener('click', () => {
-  fetch('/data')
-    .then(response => response.text())
-    .then(data => dataEl.textContent = data)
-    .catch(error => console.error(error));
-});
